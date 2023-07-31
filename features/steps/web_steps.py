d07a8f3813e04cbb195569d666ed442ea6d772d5
Sohail Hodarkar <sohailhodarkar11@gmail.com>
2023-07-31 19:22:22 -0400

    Admin UI Initial Commit
    Changed base image in .devcontainer/Dockerfile to rofrano/pipeline-selenium,
    Changed the port for the app in .devcontainer/docker-compose.yml from 8000 to 8080,
    Added BDD requirements to requirements.txt
    Added features/{steps/web_steps.py, steps/wishlists_steps.py, environment.py, wishlists.feature}

---
+++ features/steps/web_steps.py
@@ -0,0 +1,48 @@
+######################################################################
+# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+######################################################################
+
+# pylint: disable=function-redefined, missing-function-docstring
+# flake8: noqa
+"""
+Web Steps
+
+Steps file for web interactions with Selenium
+
+For information on Waiting until elements are present in the HTML see:
+    https://selenium-python.readthedocs.io/waits.html
+"""
+import logging
+from behave import when, then
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+ID_PREFIX = 'wishlist_'
+
+@when('I visit the "Home Page"')
+def step_impl(context):
+    """ Make a call to the base URL """
+    context.driver.get(context.base_url)
+
+@then('I should see "{message}" in the title')
+def step_impl(context, message):
+    """ Check the document title for a message """
+    assert(message in context.driver.title)
+
+@then('I should not see "{text_string}"')
+def step_impl(context, text_string):
+    element = context.driver.find_element(By.TAG_NAME, 'body')
+    assert(text_string not in element.text)
---
+++ features/steps/wishlists_steps.py
@@ -0,0 +1,52 @@
+#####################################################################
+# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+######################################################################
+
+"""
+Wishlist Steps
+
+Steps file for wishlists.feature
+
+For information on Waiting until elements are present in the HTML see:
+    https://selenium-python.readthedocs.io/waits.html
+"""
+import requests
+from behave import given
+
+# HTTP Return Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_204_NO_CONTENT = 204
+
+@given('the following wishlists')
+def step_impl(context):
+    """ Delete all Wishlists and load new ones """
+
+    # List all of the wishlists and delete them one by one
+    rest_endpoint = f"{context.base_url}/wishlists"
+    context.resp = requests.get(rest_endpoint)
+    assert(context.resp.status_code == HTTP_200_OK)
+    for wishlist in context.resp.json():
+        context.resp = requests.delete(f"{rest_endpoint}/{wishlist['id']}")
+        assert(context.resp.status_code == HTTP_204_NO_CONTENT)
+
+    # Load the database with new wishlists
+    for row in context.table:
+        payload = {
+            "user_id": int(row['user_id']),
+            "wishlist_name": row['wishlist_name']
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert(context.resp.status_code == HTTP_201_CREATED)
